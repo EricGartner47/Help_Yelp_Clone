f1c360d84db3c8da90ca6a4d2cd8974c2766aaf3
Your Name <email@example.com>
2021-12-03 18:42:32 -0500

    error handling

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const app = express();
 
@@ -17,25 +18,58 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
 
+
 // Security Middleware
 if (!isProduction) {
     // enable cors only in development
     app.use(cors());
-  }
-  // helmet helps set a variety of headers to better secure your app
-  app.use(helmet({
+}
+// helmet helps set a variety of headers to better secure your app
+app.use(helmet({
     contentSecurityPolicy: false
-  }));
+}));
 
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
+// Set the _csrf token and create req.csrfToken method
+app.use(
     csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
     })
-  );
+    );
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+});
+
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+});
+
+
+
 
 module.exports = app;
