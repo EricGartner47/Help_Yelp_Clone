df41d474549245bf4239f03388494041330c82d7
EricGartner47 <egartner47@gmail.com>
2022-02-14 17:51:04 -0500

    added modals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
+import { ModalProvider } from './components/context/modal';
 import './index.css';
 import App from './App';
 import configureStore from './store';
@@ -22,7 +23,9 @@ function Root() {
   return (
     <ReduxProvider store={store}>
       <BrowserRouter>
-        <App />
+        <ModalProvider>
+          <App />
+        </ModalProvider>
       </BrowserRouter>
     </ReduxProvider>
   );
